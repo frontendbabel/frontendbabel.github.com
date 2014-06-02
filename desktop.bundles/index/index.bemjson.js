1035db50ca84e3082d1a7fc93750ac2b3acd9886
Varya Stepanova <mail@varya.me>
2014-06-02 22:35:27 +0400

    Site menu in header

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -14,107 +14,33 @@
                 {
                     block: 'logo',
                     content: 'Frontend Babel'
-                }
-            ]
-        },
-        {
-            block: 'layout',
-            content: [
-                {
-                    block: 'main',
-                    content: [
-                        {
-                            block: 'article',
-                            title: 'The Case of The Wonderful Button',
-                            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                        },
-                        {
-                            block: 'slide'
-                        },
-                        {
-                            block: 'recent-posts'
-                        },
-                        {
-                            block: 'comments'
-                        },
-                        {
-                            block: 'related',
-                            content: 'xxx'
-                        }
-                    ]
                 },
                 {
-                    block: 'sidebar',
+                    block: 'site-menu',
                     content: [
                         {
-                            block: 'menu-vert',
-                            content: [
-                                {
-                                    elem: 'title',
-                                    mix: [{ block: 'box', elem: 'title'}],
-                                    content: 'On this site'
-                                },
-                                {
-                                    elem: 'item',
-                                    content: {
-                                        block: 'link',
-                                        url: '#',
-                                        content: 'Blog'
-                                    }
-                                },
-                                {
-                                    elem: 'item',
-                                    content: {
-                                        block: 'link',
-                                        url: '#',
-                                        content: 'Articles and talks'
-                                    }
-                                },
-                                {
-                                    elem: 'item',
-                                    content: {
-                                        block: 'link',
-                                        url: '#',
-                                        content: 'About me'
-                                    }
-                                }
-                            ]
+                            elem: 'item',
+                            mods: {
+                                current: true
+                            },
+                            content: {
+                                block: 'link',
+                                url: '#',
+                                content: 'Index'
+                            }
                         },
                         {
-                            block: 'github',
-                            js: { user: 'varya' }
-                        }
-                    ]
-                },
-                {
-                    block: 'prompting',
-                    content: [
-                        {
-                            block: 'languages'
-                        },
-                        {
-                            block: 'social-ico',
-                            url: '#'
+                            elem: 'item',
+                            content: {
+                                block: 'link',
+                                url: '#',
+                                content: 'About'
+                            }
                         }
                     ]
                 }
             ]
         },
-        {
-            block: 'footer',
-            content: [
-                {
-                    elem: 'left'
-                },
-                {
-                    elem: 'center'
-                },
-                {
-                    elem: 'right'
-                }
-            ]
-        },
-        /*{ block: 'custom-fonts' },*/
         { elem: 'js', url: 'index.min.js' }
     ]
 })
