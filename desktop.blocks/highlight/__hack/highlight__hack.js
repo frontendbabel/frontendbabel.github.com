57f635c904b98b0a084cbc14286bd7283b459428
Varya Stepanova <mail@varya.me>
2014-06-20 00:19:48 +0400

    Use solarized code scheme

--- desktop.blocks/highlight/__hack/highlight__hack.js
+++ desktop.blocks/highlight/__hack/highlight__hack.js
@@ -3,7 +3,7 @@ modules.require(['jquery'], function($) {
     $(function(){
         $('.highlight').each(function(i, block){
             var $block = $(block);
-            $block.addClass('i-bem').addClass('highlight_theme_github');
+            $block.addClass('i-bem').addClass('highlight_theme_solarized-light');
             $block.attr('data-bem', JSON.stringify({ highlight: {} }));
         });
     })
